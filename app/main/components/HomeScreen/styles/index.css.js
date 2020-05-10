ab073e95a767c9349e45d39525a80e81b37c3084
phucnhb <phucnhb@bkav.com>
2020-05-10 15:30:04 +0700

    Chinh sua css

--- app/main/components/HomeScreen/styles/index.css.js
+++ app/main/components/HomeScreen/styles/index.css.js
@@ -301,7 +301,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingHorizontal: 16,
-    paddingTop: isIPhoneX ? 36 : 12,
+    paddingTop: isIPhoneX ? 36 : 8,
     zIndex: 99,
   },
 
